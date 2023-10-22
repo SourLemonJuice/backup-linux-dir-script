d63723409488ec84a8d258802d79890695becc27
SourLemonJuice <null>
2023-10-22 08:29:25 +0800

    修正过滤列表,直接排除了文件夹而非里面的文件(笨蛋)

--- src/config.bash
+++ src/config.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-BackupFolder=../temp/back
+BackupFolder=../temp/backup
 RootPath=../temp/test
 NeedRoot=0
-# Tar_Path='--exclude=./lost+found/* --exclude=./sys/* --exclude=./proc/* --exclude=./boot/* --exclude=./dev/* --exclude=./mnt/*'
