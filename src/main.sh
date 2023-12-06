9d35b0f631e8af95b9b23578335d8fb838428e3b
SourLemonJuice <null>
2023-12-07 01:14:45 +0800

    将检测参数的逻辑改为两个case检测，后者在开始前初始化备份仓库文件夹 在main.sh里添加了超多的log项，谁叫弄上一行的功能的时候出bug了呢

--- src/main.sh
+++ src/main.sh
@@ -23,38 +23,71 @@ fi
     LogPath=$(realpath -m $LogPath)
 }
 # 日志函数
-source $ShellFilePath/lib/logger.sh && logger file "已加载日志函数"
+source $ShellFilePath/lib/logger.sh
 # 初始化日志文件
 logger 'init'
+logger 'file' "已初始化 日志函数"
 
 # 加载一些小东西
 # 输出数组内容
-source $ShellFilePath/lib/println_array_items.sh && logger file "已加载读出数组函数"
+source $ShellFilePath/lib/println_array_items.sh && logger file "已加载 读出数组函数"
 # 打印分割线
-source $ShellFilePath/lib/separator.sh && logger file "已加载分割线函数"
+source $ShellFilePath/lib/separator.sh && logger file "已加载 分割线函数"
 # 读取初始化函数
-source $ShellFilePath/init.sh && logger file "已加载初始化函数"
+source $ShellFilePath/init.sh && logger file "已加载 初始化函数"
 # 读取备份逻辑函数
-source $ShellFilePath/backup.sh && logger file "已加载备份逻辑函数"
+source $ShellFilePath/backup.sh && logger file "已加载 备份逻辑函数"
 # 读取恢复函数
-source $ShellFilePath/restore.sh && logger file "已加载恢复逻辑函数"
+source $ShellFilePath/restore.sh && logger file "已加载 恢复逻辑函数"
 
 # 获取参数
 Options=$(getopt -o vhBRzf -l version,help,backup,restore,full-backup -- "$@")
 # 获取失败则退出
 [ ! $? -eq 0 ] && logger both "参数格式错误" "参数格式错误 $@" && exit 1
+# 记录处理后用户输入的参数
+logger 'file' "整理输入参数成功，整理后用户输入的参数列表为 [$@]"
 # 格式化getopt的输出
 eval set -- "$Options" || exit 1
 
-# 主程序
-logger 'file' "函数加载完成"
 # 这个循环只应该检测到一次可以执行的项，在每个项后面都应该写上 break 或 exit
+logger 'file' "进入第一个环境的循环"
+while true;
+do
+    case $1 in
+        -h | --help)
+            logger 'file' "打印帮助信息"
+            cat $ShellFilePath/help_info
+            exit
+        ;;
+        -v | --version)
+            logger 'file' "打印版本号"
+            cat $ShellFilePath/version
+            exit
+        ;;
+        --)
+            logger 'file' "无参数输入，打印帮助信息"
+            # 打印一个提示信息
+            cat $ShellFilePath/help_info
+            exit
+        ;;
+        *)
+            logger 'file' "参数不是小功能，准备进入第二个环境的循环"
+            break
+        ;;
+    esac
+done
+
+logger 'file' "进入第二个环境的循环"
+
+# 初始化需要备份的仓库位置
+init $@ && logger 'file' "init函数执行"
+# 这里还是用break吧，说不定回头会在下面干什么呢
 while true
 do
     case $1 in
         -B | --backup)
-            init $@ && logger 'file' "init函数执行"
             # 普通备份模式
+            logger 'file' "进入普通备份模式"
             # 第一个判断检测当前是否有已经存在的备份组，第二个判断读取配置文件来决定是否强制完全备份
             if [[ -f $BackupFolder/$Now_Backup/.tar_snapshot ]] && [[ $Tar_Default_Full_Backup -eq 0 ]]
             then
@@ -65,33 +98,19 @@ do
             break
         ;;
         -F | --full-backup)
-            init $@ && logger 'file' "init函数执行"
-            # 完整备份模式
+            # 强制完整备份模式
+            logger 'file' "进入强制完整备份模式"
             backup 'full' $2
             break
         ;;
         -R | --restore)
-            init $@ && logger 'file' "init函数执行"
+            logger 'file' "进入恢复模式"
             # 调用备份函数
             restore $2
             break
         ;;
-        -h | --help)
-            cat $ShellFilePath/help_info
-            exit
-        ;;
-        -v | --version)
-            cat $ShellFilePath/version
-            exit
-        ;;
-        --)
-            echo "没有执行任何参数"
-            # 打印一个提示信息
-            cat $ShellFilePath/help_info
-            exit
-        ;;
-        ?)
-            echo "未知参数 $1"
+        *)
+            logger 'file' "第二个循环输入了未知参数 $1"
             exit 1
         ;;
     esac
